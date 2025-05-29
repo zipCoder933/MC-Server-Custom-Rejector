fb78bd1e3d65dc8feacb4ef9959c230de14d2f24
zipCoder933 <67439766+zipCoder933@users.noreply.github.com>
2025-05-28 21:41:34 -0600

    Changed it to 1.20.1

--- src/main/java/org/zipcoder/serverBlocker/PluginConfig.java
+++ src/main/java/org/zipcoder/serverBlocker/PluginConfig.java
@@ -3,5 +3,5 @@ package org.zipcoder.serverBlocker;
 public class PluginConfig {
     public String discord_webhook_url = "https://discord.com/api/webhooks/1377479606822506656/F8vv6GxZrJ8VAs8sO7ZKAlRTKDHOaqRP2LJrjZgDPY3iw4WLf24SUFJc8aFL1UYg3Uen";
     public String custom_message = "Hang tight, the server is starting up";
-    public String server_name = "Vanilla";
+    public String server_name = "Nexus";
 }
