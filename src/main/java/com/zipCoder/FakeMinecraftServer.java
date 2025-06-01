d37d8cff04b5d979e77dd96ef02e0c73b1fcab42
zipCoder933 <67439766+zipCoder933@users.noreply.github.com>
2025-06-01 09:03:20 -0600

    a

--- src/main/java/com/zipCoder/FakeMinecraftServer.java
+++ src/main/java/com/zipCoder/FakeMinecraftServer.java
@@ -6,7 +6,7 @@ import java.nio.charset.StandardCharsets;
 
 public class FakeMinecraftServer {
 
-    static Config config = Config.loadConfig();
+//    static Config config = Config.loadConfig();
     static String version = "watcher v1.3.0";
 
 //    private static void packetLog(String message) {
@@ -19,110 +19,110 @@ public class FakeMinecraftServer {
 //        java.util.concurrent.Executors.newSingleThreadScheduledExecutor().scheduleAtFixedRate(
 //                FakeMinecraftServer::handleMemory, 0, 1, java.util.concurrent.TimeUnit.MINUTES);
 
-        for (Server server : config.servers) {
-            (new Thread(() -> runServer(server))).start();
-        }
+//        for (Server server : config.servers) {
+//            (new Thread(() -> runServer(server))).start();
+//        }
     }
 
-    public static void runServer(Server server) {
-        try (ServerSocket serverSocket = new ServerSocket(server.port)) {
-            System.out.println("Fake server running on port " + server.port);
-
-            while (true) {
-                handleMemory();
-                try (Socket socket = serverSocket.accept()) {
-                    System.out.println("Connection from " + socket.getInetAddress());
-
-                    DataInputStream in = new DataInputStream(socket.getInputStream());
-                    DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-
-                    // === Handshake Packet ===
-                    int packetLength = readVarInt(in);
-                    //packetLog("\tPacket Length: " + packetLength);
-                    int packetId = readVarInt(in);  // Should be 0 (Handshake)
-                    //packetLog("\tPacket ID: " + packetId);
-
-                    //We can ignore packets we dont care about to prevent errors
-                    if (packetId != 0 && packetId != 1) return;
-                    /**
-                     * Handshake (packet ID 0) — to get the protocol version and next state,
-                     * Status packets (e.g., status request ID 0, ping ID 1),
-                     * Player connection packets (usually Login Start packet with a specific ID),
-                     */
-
-                    int protocolVersion = readVarInt(in);  // Protocol version
-                    //packetLog("\tProtocol Version: " + protocolVersion);
-                    String serverAddress = readString(in);
-                    //packetLog("\tServer Address: " + serverAddress);
-                    int serverPort = in.readUnsignedShort();
-                    //packetLog("\tServer Port: " + serverPort);
-                    int nextState = readVarInt(in);  // 1 = status, 2 = login
-                    //packetLog("\tNext State: " + nextState);
-
-
-                    if (nextState == 1) { // status request
-                        if (config.handleStatusRequests) respondWithStatus(server, in, out, protocolVersion);
-                    } else if (nextState == 2) { //Login request
-
-                        // === Login Start Packet ===
-                        packetLength = readVarInt(in);
-                        packetId = readVarInt(in);  // Should be 0 (Login Start)
-                        String username = readString(in);
-                        //packetLog("\tUser: " + username);
-
-                        // === Send Login Disconnect ===
-                        try {
-                            String json = "{\"text\":\"" + config.rejectMessage + "\"}";
-                            ByteArrayOutputStream buffer = new ByteArrayOutputStream();
-                            DataOutputStream packet = new DataOutputStream(buffer);
-                            packet.writeByte(0x00); // Login Disconnect packet ID
-                            writeString(packet, json);
-                            sendPacket(out, buffer.toByteArray());
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        }
-
-                        // === Send discord webhook ===
-                        try {
-                            DiscordWebhook webhook = new DiscordWebhook(config.discordWebhook);
-                            webhook.setUsername("Watcher App");
-                            webhook.setContent("Player **" + username + "** wants to join server **" + server.name + "**");
-                            webhook.execute();
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        }
-                    }
-
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-
-
-        } catch (Exception e) {
-            try {//Print/send error message
-                String message = e.getMessage() == null ? "Unknown error" : e.getMessage();
-                DiscordWebhook webhook = new DiscordWebhook(config.discordWebhook);
-                webhook.setUsername("Watcher App");
-                String content = "Error running server (restarting) " + server.port + ": " + message + "\n";
-                try {
-                    for (StackTraceElement element : e.getStackTrace()) content += element.toString() + "\n";
-                } catch (Exception ignored) {
-                }
-                System.out.println(content);
-                webhook.setContent(content);
-                webhook.execute();
-            } catch (Exception ignored) {
-            }
-
-            try {//wait
-                Thread.sleep(30000);
-            } catch (InterruptedException ex) {
-            }
-
-            runServer(server); //restart
-        }
-    }
+//    public static void runServer(Server server) {
+//        try (ServerSocket serverSocket = new ServerSocket(server.port)) {
+//            System.out.println("Fake server running on port " + server.port);
+//
+//            while (true) {
+//                handleMemory();
+//                try (Socket socket = serverSocket.accept()) {
+//                    System.out.println("Connection from " + socket.getInetAddress());
+//
+//                    DataInputStream in = new DataInputStream(socket.getInputStream());
+//                    DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+//
+//                    // === Handshake Packet ===
+//                    int packetLength = readVarInt(in);
+//                    //packetLog("\tPacket Length: " + packetLength);
+//                    int packetId = readVarInt(in);  // Should be 0 (Handshake)
+//                    //packetLog("\tPacket ID: " + packetId);
+//
+//                    //We can ignore packets we dont care about to prevent errors
+//                    if (packetId != 0 && packetId != 1) return;
+//                    /**
+//                     * Handshake (packet ID 0) — to get the protocol version and next state,
+//                     * Status packets (e.g., status request ID 0, ping ID 1),
+//                     * Player connection packets (usually Login Start packet with a specific ID),
+//                     */
+//
+//                    int protocolVersion = readVarInt(in);  // Protocol version
+//                    //packetLog("\tProtocol Version: " + protocolVersion);
+//                    String serverAddress = readString(in);
+//                    //packetLog("\tServer Address: " + serverAddress);
+//                    int serverPort = in.readUnsignedShort();
+//                    //packetLog("\tServer Port: " + serverPort);
+//                    int nextState = readVarInt(in);  // 1 = status, 2 = login
+//                    //packetLog("\tNext State: " + nextState);
+//
+//
+//                    if (nextState == 1) { // status request
+//                        if (config.handleStatusRequests) respondWithStatus(server, in, out, protocolVersion);
+//                    } else if (nextState == 2) { //Login request
+//
+//                        // === Login Start Packet ===
+//                        packetLength = readVarInt(in);
+//                        packetId = readVarInt(in);  // Should be 0 (Login Start)
+//                        String username = readString(in);
+//                        //packetLog("\tUser: " + username);
+//
+//                        // === Send Login Disconnect ===
+//                        try {
+//                            String json = "{\"text\":\"" + config.rejectMessage + "\"}";
+//                            ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+//                            DataOutputStream packet = new DataOutputStream(buffer);
+//                            packet.writeByte(0x00); // Login Disconnect packet ID
+//                            writeString(packet, json);
+//                            sendPacket(out, buffer.toByteArray());
+//                        } catch (Exception e) {
+//                            e.printStackTrace();
+//                        }
+//
+//                        // === Send discord webhook ===
+//                        try {
+//                            DiscordWebhook webhook = new DiscordWebhook(config.discordWebhook);
+//                            webhook.setUsername("Watcher App");
+//                            webhook.setContent("Player **" + username + "** wants to join server **" + server.name + "**");
+//                            webhook.execute();
+//                        } catch (Exception e) {
+//                            e.printStackTrace();
+//                        }
+//                    }
+//
+//                } catch (Exception e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//
+//
+//        } catch (Exception e) {
+//            try {//Print/send error message
+//                String message = e.getMessage() == null ? "Unknown error" : e.getMessage();
+//                DiscordWebhook webhook = new DiscordWebhook(config.discordWebhook);
+//                webhook.setUsername("Watcher App");
+//                String content = "Error running server (restarting) " + server.port + ": " + message + "\n";
+//                try {
+//                    for (StackTraceElement element : e.getStackTrace()) content += element.toString() + "\n";
+//                } catch (Exception ignored) {
+//                }
+//                System.out.println(content);
+//                webhook.setContent(content);
+//                webhook.execute();
+//            } catch (Exception ignored) {
+//            }
+//
+//            try {//wait
+//                Thread.sleep(30000);
+//            } catch (InterruptedException ex) {
+//            }
+//
+//            runServer(server); //restart
+//        }
+//    }
 
     private static long totalMemory, freeMemory, usedMemory, maxMemory;
     private static double memoryPercent;
